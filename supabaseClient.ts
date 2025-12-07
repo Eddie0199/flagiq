d98a64389ca1efcd16d0c9b348e294a5e8643a21
Eddie0199 <eddie_rawle@hotmail.com>
2025-12-07 17:19:56 +0100

    Create supabaseClient.ts

---
+++ supabaseClient.ts
@@ -0,0 +1,11 @@
+// src/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY as string;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.warn('Supabase env vars are missing');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
