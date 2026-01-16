f7e31ce7b84ea99039ffbd6806bb0ec9c18d5f0a
Eddie0199 <eddie_rawle@hotmail.com>
2026-01-16 20:08:19 +0100

    Update Capacitor bundle ID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.flaggame.app',
-  appName: 'Flag Game',
+  appId: 'com.wildmoustachegames.flagiq',
+  appName: 'FlagIQ',
   webDir: 'build',
   bundledWebRuntime: false,
   server: {
