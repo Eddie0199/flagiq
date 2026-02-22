1ffde4fa1ef0a407db1277380bcf2cef5536561f
Eddie0199 <eddie_rawle@hotmail.com>
2026-02-22 22:16:19 +0100

    Fix StoreKit storefront notification compile error

--- ios/App/App/StoreKitPurchasePlugin.swift
+++ ios/App/App/StoreKitPurchasePlugin.swift
@@ -27,7 +27,7 @@ public class StoreKitPurchasePlugin: CAPPlugin, SKProductsRequestDelegate, SKPay
             NotificationCenter.default.addObserver(
                 self,
                 selector: #selector(handleStorefrontDidChangeNotification(_:)),
-                name: NSNotification.Name.SKPaymentQueueDidChangeStorefront,
+                name: Notification.Name("SKPaymentQueueDidChangeStorefront"),
                 object: SKPaymentQueue.default()
             )
         }
